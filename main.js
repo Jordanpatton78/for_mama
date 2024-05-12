7f6903b1858abe5da642fd3ea8de692033120435
Jordanpatton78 <44303813+Jordanpatton78@users.noreply.github.com>
2024-05-11 21:14:59 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+const menu = document.getElementsByClassName("nav-menu")[0];
+const navigation = document.getElementById("navigation");
+const hamburger = document.getElementsByClassName("menu-ham");
+const button = document.getElementById("button");
+
+menu.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    for (let i = 0; i < hamburger.length; i++) {
+        hamburger[i].classList.toggle("active");
+    }
+    navigation.classList.toggle("open");
+}
